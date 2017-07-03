06eb3479ca491f116ef81b40554be36ffbfe15d3
Freddy Juhel <domaine75@gmail.com>
2017-07-03 23:55:25 +0200

    Initial commit

---
+++ GetQuote/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetQuote
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Create a request for the URL. 
+      WebRequest request = WebRequest.Create(
+        "http://www.badnuke.com/");
+      // If required by the server, set the credentials.
+      request.Credentials = CredentialCache.DefaultCredentials;
+      // Get the response.
+      WebResponse response = request.GetResponse();
+      // Display the status.
+      Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+      // Get the stream containing content returned by the server.
+      Stream dataStream = response.GetResponseStream();
+      // Open the stream using a StreamReader for easy access.
+      StreamReader reader = new StreamReader(dataStream);
+      // Read the content.
+      string responseFromServer = reader.ReadToEnd();
+      // Display the content.
+      //Console.WriteLine(responseFromServer);
+      // search for  <div class="text-center">
+
+      Console.ReadKey();
+      // Clean up the streams and the response.
+      reader.Close();
+      response.Close();
+    }
+  }
+}
