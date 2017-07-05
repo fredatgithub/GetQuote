84dbd98d552cf593a8295671b010b6f600591c85
Freddy Juhel <domaine75@gmail.com>
2017-07-05 18:03:43 +0200

    Add a project to format quotes into xml format

---
+++ CreateXMLFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CreateXMLFile
+{
+  internal static class Program
+  {
+    private static void Main()
+    {
+      const string fileName = "quotes.txt";
+      Dictionary<string, string> dicoQuotes = new Dictionary<string, string>();
+      List<string> quotesList = new List<string>();
+      StreamReader sr = new StreamReader(fileName);
+      string line = string.Empty;
+      while ((line = sr.ReadLine()) != null)
+      {
+        //Console.WriteLine(line);
+        quotesList.Add(line);
+      }
+    }
+  }
+}
